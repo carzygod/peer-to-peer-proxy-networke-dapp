312040afaac49638aa8455fe469b3bbe640e6506
carzygod <2373250856@qq.com>
2021-07-24 10:20:24 +0800

    🔥尝试复活

--- lib/utils/http_util.dart
+++ lib/utils/http_util.dart
@@ -34,7 +34,7 @@ class HttpUtil {
       await SharedPreferencesUtil.getInstance()
           .getString(AppStrings.TOKEN)
           .then((token) {
-        options.headers[AppStrings.TOKEN] = "Bearer " + token;
+        options.headers[AppStrings.TOKEN] = token;
         print("token=$token");
       });
       dio.unlock();
